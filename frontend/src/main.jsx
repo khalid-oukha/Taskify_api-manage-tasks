32c20ee970207178cf8edeaca19e54d547a538dd
khalid oukha <oukhakhalid@gmail.com>
2024-03-29 12:08:42 +0000

    Add index.css and main.jsx files

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.jsx'
+import { ContextHelper } from './context/ContextHelper.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContextHelper>
+      <RouterProvider router={router} />
+    </ContextHelper>
+  </React.StrictMode>,
+)
